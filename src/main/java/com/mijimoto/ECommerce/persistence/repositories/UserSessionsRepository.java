e43108f0829bcc042fe4860f983a018c7aac6520
mijimoto <mijimotos@gmail.com>
2025-10-24 23:31:13 +0700

    add entity+repo for UserSession and UserToken

---
+++ src/main/java/com/mijimoto/ECommerce/persistence/repositories/UserSessionsRepository.java
@@ -0,0 +1,23 @@
+/*
+ * Created on 2025-10-24 ( 23:12:32 )
+ * Generated manually following Telosys conventions
+ */
+package com.mijimoto.ECommerce.persistence.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+import com.mijimoto.ECommerce.persistence.entities.UserSessions;
+
+/**
+ * Spring Data JPA repository for entity "UserSessions"
+ *
+ * Provides basic CRUD and custom finders for sessions.
+ */
+public interface UserSessionsRepository extends JpaRepository<UserSessions, Integer> {
+
+    Optional<UserSessions> findBySessionUuid(String sessionUuid);
+
+    List<UserSessions> findByUsers_IdAndIsActiveTrue(Integer userId);
+}
